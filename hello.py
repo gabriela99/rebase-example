54c565d1036b18f6d3d4ead9e91e2928738fe2e6
Martin Dreher <luma.inc@gmail.com>
2018-10-05 09:31:37 +0200

    remove obsolete greeting

--- hello.py
+++ hello.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
-def greeting():
-    print('hello world')
-
 def ask_for_name():
     name = input('please enter your name: ')
     print('hi', name)
 
 if __name__ == '__main__':
-    greeting()
     ask_for_name()
