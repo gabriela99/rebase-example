348cb5adf3079d75325de2f86916efc648f5795c
Martin Dreher <luma.inc@gmail.com>
2018-10-05 09:30:52 +0200

    say the current time

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+from datetime import datetime
+
+def say_time():
+    print('it is', datetime.now())
 
 def ask_for_name():
     name = input('please enter your name: ')
@@ -6,4 +10,9 @@ def ask_for_name():
     print('have a nice day!')
 
 if __name__ == '__main__':
+<<<<<<< HEAD
     ask_for_name()
+=======
+    greeting()
+    say_time()
+>>>>>>> say the current time
