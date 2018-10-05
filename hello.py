ed59beb678c0f3e164af21da2442fe2eb68c2529
Martin Dreher <luma.inc@gmail.com>
2018-10-05 09:27:36 +0200

    feature: ask for name

--- hello.py
+++ hello.py
@@ -3,5 +3,10 @@
 def greeting():
     print('hello world')
 
+def ask_for_name():
+    name = input('please enter your name: ')
+    print('hi', name)
+
 if __name__ == '__main__':
     greeting()
+    ask_for_name()
